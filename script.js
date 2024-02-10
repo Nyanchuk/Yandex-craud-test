3ed1ac6a31adfa0eab871002576d252bc62b12cf
global_gifts <fifa.1998.sava@mail.ru>
2024-02-10 13:25:47 +1000

    main_page_center

---
+++ script.js
@@ -0,0 +1,14 @@
+// const runningText = document.getElementById('running-text');
+// let text = runningText.textContent;
+// let position = 0;
+
+// function runText() {
+//     runningText.textContent = text.substring(position) + text.substring(0, position);
+//     position++;
+//     if (position > text.length) {
+//         position = 0;
+//     }
+//     requestAnimationFrame(runText);
+// }
+
+// requestAnimationFrame(runText);
